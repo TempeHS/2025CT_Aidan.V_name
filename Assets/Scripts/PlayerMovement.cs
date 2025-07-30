97386d7b56f7741ed246a491de15e4a1bdf923c0
Aidan V <159743307+AidanVu8@users.noreply.github.com>
2025-07-30 02:49:46 +0000

    added vertical variable

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
+    private float vertical;
     private float speed = 8f;
     private float jumpingPower = 16f;
     private bool isFacingRight = true;
